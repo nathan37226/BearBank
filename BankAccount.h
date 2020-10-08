b38c230c291d74041adfd195065ce9e68080bbfc
Nathan Obert <nathan829@live.missouristate.edu>
2020-10-08 12:39:36 -0500

    Created private funct 'round', delcared some functions

--- BankAccount.h
+++ BankAccount.h
@@ -4,6 +4,7 @@ Group Members: Nathan Obert M03134502, Keegan Maynard
 #ifndef BANKACCOUNT_H
 #define BANKACCOUNT_H
 #include <string>
+#include <cmath>
 using namespace std;
 
 class BankAccount
@@ -14,13 +15,47 @@ private:
     double intRate;
     double serCharge;
 
+    double round(double value, int decimal);
+
 public:
     BankAccount(string actNum, double bal, double rate);
-    virtual void deposit(double amount);
-    virtual void withdraw(double amount);
+    virtual void deposit(double amount) = 0;
+    virtual void withdraw(double amount) = 0;
     void calcInt();
     void yearlyCharge();
     void closeAct();
 
 };
+
+BankAccount::BankAccount(string actNum, double bal, double rate)
+{
+    accountNum = actNum;
+    balance = bal;
+    intRate = rate;
+}
+
+double BankAccount::round(double value, int decimal)
+{
+    double multiplier = pow(10, decimal);
+    value = value * multiplier; //makes so the integer part will have all the numbers wanted
+
+    int tempValue = value; //cuts off extra decimal part
+    int higherTempValue = tempValue + 1; //in case it needed to be rounded to the next higher number
+
+    double firstDist = abs(value - tempValue);  //finding distance from value to lower rounding
+    double secondDist = abs(value - higherTempValue); //distance from value to higher rounding
+
+    tempValue = (firstDist < secondDist) ? tempValue : higherTempValue; //returns value w smaller distance, i.e. the closest one
+    value = tempValue / multiplier; //dividing to get real decimal
+
+    return value;
+}
+
+void BankAccount::calcInt()
+{
+    double dailyIntRate = intRate / 365; //maybe change in future to account for leap years
+    double dailyInt = dailyIntRate * balance;
+    balance = round(balance + dailyInt, 2); //private member function to round to a given amount of decimals
+}
+
 #endif
