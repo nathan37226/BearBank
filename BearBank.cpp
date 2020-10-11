0a70fb885a07630ef95765fcb9e970d66dd0c9bf
Nathan Obert <nathan829@live.missouristate.edu>
2020-10-11 16:52:06 -0500

    Testing

--- BearBank.cpp
+++ BearBank.cpp
@@ -3,11 +3,25 @@ Group Members: Nathan Obert M03134502, Keegan Maynard
 */
 #include <iostream>
 #include "CheckingAccount.h"
+#include "OS.h"
 #include <string>
+#include <vector>
+
+
 
 int main()
 {
-    CheckingAccount chk = CheckingAccount("C0001", 50.00, 2.00);
-    chk.deposit(10000.00);
+    CheckingAccount acc1 = CheckingAccount("C001", 50.05, 1.02);
+    Accounts actSet1;
+    actSet1.chk = acc1;
+    CheckingAccount acc2 = CheckingAccount("C002", 100.05, 2.00);
+    Accounts actSet2;
+    actSet2.chk = acc2;
+    CheckingAccount acc3 = CheckingAccount("C003", 200.99, 1.95);
+    Accounts actSet3;
+    actSet3.chk = acc3;
+
+    vector<Accounts> acctVect;
+
     return 0;
 }
