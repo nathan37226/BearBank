14c7ac86d821de4a6848f4e9bcb15bd7c90058f1
KeeganMaynard <66282855+KeeganMaynard@users.noreply.github.com>
2020-10-11 16:34:05 -0500

    header file deposite and declarations

--- SavingsAccount.h
+++ SavingsAccount.h
@@ -9,6 +9,36 @@ class SavingsAccount : public BankAccount
 {
 private:
 	string status;
-	void setStatus;
+	void setStatus();
+public:
+	SavingsAccount(string actNum, double bal, double rate);
+	void deposit(double amount);
+	void withdraw(double amount);
+	void closeAcc();
 };
+
+SavingsAccount::SavingsAccount(string actNum, double bal, double rate) : BankAccount(actNum, bal, rate)
+{
+	;
+}
+
+void SavingsAccount::deposit(double amount)
+{
+	try
+	{
+		if (amount < 0.01)
+		{
+			string error = "Invalid arguement: you cannot deposit a negative amount";
+			throw error;
+		}
+		else
+		{
+			setBal(getBal() + amount);
+		}
+	}
+	catch (string err)
+	{
+		cout << err << endl;
+	}
+}
 #endif
