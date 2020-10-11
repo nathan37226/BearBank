5ab11d9d0b8bd713bcc91fbc69657cc9781ad05a
Nathan Obert <nathan829@live.missouristate.edu>
2020-10-11 16:50:23 -0500

    Initial header with struct

---
+++ OS.h
@@ -0,0 +1,17 @@
+#ifndef OS_H
+#define OS_H
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "CheckingAccount.h"
+//#include "SavingsAccount.h"
+using namespace std;
+
+struct Accounts
+{
+    CheckingAccount chk;
+    //SavingsAccount sav;
+};
+
+#endif
