771ee4b07842694afb6b87b0739c5395189c3b23
Nathan Obert <nathan829@live.missouristate.edu>
2020-10-12 16:57:58 -0500

    Reading from file to create accounts worksgit statusgit status

--- BearBank.cpp
+++ BearBank.cpp
@@ -2,7 +2,6 @@
 Group Members: Nathan Obert M03134502, Keegan Maynard 
 */
 #include <iostream>
-#include "CheckingAccount.h"
 #include "OS.h"
 #include <string>
 #include <vector>
@@ -11,7 +10,12 @@ Group Members: Nathan Obert M03134502, Keegan Maynard
 
 int main()
 {
-    CheckingAccount acc1 = CheckingAccount("C001", 50.05, 1.02);
+
+    vector<Accounts> acctVect = {};
+
+    acctVect = getInfo();
+
+    /*CheckingAccount acc1 = CheckingAccount("C001", 50.05, 1.02);
     Accounts actSet1;
     actSet1.chk = acc1;
     CheckingAccount acc2 = CheckingAccount("C002", 100.05, 2.00);
@@ -19,11 +23,15 @@ int main()
     actSet2.chk = acc2;
     CheckingAccount acc3 = CheckingAccount("C003", 200.99, 1.95);
     Accounts actSet3;
-    actSet3.chk = acc3;
+    actSet3.chk = acc3;*/
 
-    vector<Accounts> acctVect {actSet1, actSet2, actSet3};
+    for (int i = 0; i < acctVect.size(); i++)
+    {
+        cout << acctVect[i].chk.getActNum() << " " << acctVect[i].chk.getBal() << " " << acctVect[i].chk.getRate() << endl;
+        cout << acctVect[i].sav.getActNum() << " " << acctVect[i].sav.getStatus() << " " << acctVect[i].sav.getRate() << endl;
+    }
 
-    saveInfo(acctVect);
+    //saveInfo(acctVect);
 
     return 0;
 }
--- OS.h
+++ OS.h
@@ -5,22 +5,114 @@
 #include <fstream>
 #include <vector>
 #include "CheckingAccount.h"
-//#include "SavingsAccount.h"
+#include "SavingsAccount.h"
 using namespace std;
 
 struct Accounts
 {
     CheckingAccount chk;
-    //SavingsAccount sav;
+    SavingsAccount sav;
 };
 
 string getAccountInfo(Accounts accts)
 {
-    string chkInfo = "";
+    string chkInfo = "", savInfo;
     chkInfo = accts.chk.getActNum() + " " + to_string( accts.chk.getBal() ) + " " + to_string( accts.chk.getRate() );
-    
-    return chkInfo;
 
+    if (accts.sav.getStatus() == "Permanently Closed")
+    {
+        savInfo = "CLOSED SAVINGS ACCOUNT";
+    }
+    else
+    {
+        savInfo = accts.sav.getActNum() + " " + to_string( accts.sav.getBal() ) + " " + to_string( accts.chk.getRate() );
+        savInfo += " " + accts.sav.getStatus();
+    }
+    
+
+    return chkInfo + "\n" + savInfo;
+}
+
+CheckingAccount createChkFromInfo(string info)
+{
+    string actNum = info.substr(0, info.find(" ")); //slicing out act Num
+    info = info.substr(info.find(" ") + 1, string::npos);
+    double bal = stod( info.substr(0, info.find(" ")) ); //getting balance as a double
+    info = info.substr(info.find(" ") + 1, string::npos);
+    double rate = stod( info ); //getting rate as a double
+
+    CheckingAccount acct = CheckingAccount(actNum, bal, rate); //creating account with sliced info
+
+    if (actNum.substr(actNum.length() - 1, string::npos) == "*") //seeing if High Risk or not
+    {
+        acct.setRisk(2); //ensures the priv member 'flag' is properly set
+    }
+
+    return acct; //return newly minted checking account object
+}
+
+SavingsAccount createSavFromInfo(string info)
+{
+    if (info == "CLOSED SAVINGS ACCOUNT")
+    {
+        SavingsAccount acct = SavingsAccount();
+        acct.setStatus(3);
+        return acct;
+    }
+    else
+    {    
+        string actNum = info.substr(0, info.find(" ")); //slicing out act Num
+        info = info.substr(info.find(" ") + 1, string::npos); //shorting string
+        double bal = stod( info.substr(0, info.find(" ")) ); //getting balance as a double
+        info = info.substr(info.find(" ") + 1, string::npos);
+        double rate = stod( info.substr(0, info.find(" ")) ); //getting rate as a double
+        info = info.substr(info.find(" ") + 1, string::npos);
+        //info is now just the status of the act
+        SavingsAccount acct = SavingsAccount(actNum, bal, rate); //creating return object
+
+        if (info == "Active")
+        {
+            acct.setStatus(1); //changing status member to match
+        }
+        else
+        {
+            acct.setStatus(2);
+        }
+        return acct;
+    }
+}
+
+vector<Accounts> getInfo()
+{
+    vector<Accounts> acctVect = {};
+    ifstream inputFile;
+    inputFile.open("TotallyNotBankInfo.txt");
+
+    if (!inputFile) //i.e. no bank info to read
+    {
+        return acctVect;
+    }
+
+    else  //i.e. there is bank info saved
+    {
+        string chkLine = "", savLine = "";
+
+        while (getline(inputFile, chkLine))
+        {
+            getline(inputFile, savLine);
+            Accounts accSet; //now need to copy data froms strings into struct attributes
+            CheckingAccount check = createChkFromInfo(chkLine); //creates checking act from line
+            SavingsAccount saving = createSavFromInfo(savLine); //same with next line
+
+            accSet.chk = check; //adding new accts to struct
+            accSet.sav = saving;
+
+            acctVect.push_back(accSet); //adding the struct object to the vector
+        }
+
+        inputFile.close(); //done reading, so this can be closed
+        return acctVect;
+    }
 }
 
 void saveInfo(vector<Accounts> acctVect)
@@ -30,6 +122,7 @@ void saveInfo(vector<Accounts> acctVect)
 
     for (int i=0; i < acctVect.size(); i++)
     {
+        outFile << "Account " << to_string(i) << endl;
         outFile << getAccountInfo(acctVect[i]) << endl;
     }
 
