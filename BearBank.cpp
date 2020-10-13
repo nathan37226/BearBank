3cd38e125ddae5c77c5efbea60752e355ab6a97e
Nathan Obert <nathan829@live.missouristate.edu>
2020-10-12 20:04:13 -0500

    Outlined initial switch case for main while loop

--- BearBank.cpp
+++ BearBank.cpp
@@ -9,8 +9,6 @@ using namespace std;
 
 int main()
 {   
-    cout << INITIAL_ACCT_NUM << endl;
-
     vector<Accounts> acctVect = {Accounts()}; //makes size be 1, used for increment acct Num
     acctVect = getInfo(); //gets any existing info, if present
     
@@ -19,13 +17,47 @@ int main()
     string accountInterface = "[1] Make a deposit\n[2] Make a Withdrawl";
     incrementActNum( acctVect[acctVect.size()-1].chk.getActNum() );
 
-    cout << INITIAL_ACCT_NUM << endl;
-
-    /*while (true) //main while loop
+    while (true) //main while loop
     {
         cout << mainInterface << endl;
+        string input = "";
+        int option = -1;
+        getline(cin, input);
 
-    }*/
+        try
+        {
+            if (input.length() == 1)
+                option = stoi(input); //Since stoi will make "2a" become 2, ensuring the length is 1 prevents this type of invalid input, also same with negative numbers
+            else
+                throw 1; //just throwing any int here to be caught later
+        }
+        catch (int a)
+        {
+            cout << "Invalid option!" << endl;
+            continue;
+        }
+
+        switch (option)
+        {
+            case 1: //open new accounts
+            {
+                ;
+            }
+
+            case 2: //login to accounts
+            {
+                ;
+            }
+
+            case 3: //Exit BearBank
+            {
+                ;
+            }
+        }
+
+        if (option == 3)
+            break;
+    }
 
 
 
