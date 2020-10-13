f0b5f652973aaa12d629b90f6f24b8c7c8b4fc12
Nathan Obert <nathan829@live.missouristate.edu>
2020-10-12 19:37:13 -0500

    ActNum generator function

--- BearBank.cpp
+++ BearBank.cpp
@@ -5,17 +5,29 @@ Group Members: Nathan Obert M03134502, Keegan Maynard
 #include "OS.h"
 #include <string>
 #include <vector>
-
-
+using namespace std;
 
 int main()
 {   
-    vector<Accounts> acctVect = {};
-    acctVect = getInfo();
-    string message;
-    cout << acctVect[0].chk.getBal() << endl;
-    acctVect[0].chk.deposit(420.00);
-    cout << message << endl;
-    cout << to_string(acctVect[0].chk.getBal()) << endl;
+    cout << INITIAL_ACCT_NUM << endl;
+
+    vector<Accounts> acctVect = {Accounts()}; //makes size be 1, used for increment acct Num
+    acctVect = getInfo(); //gets any existing info, if present
+    
+    string mainInterface = "[1] Open an account\n[2] Login to an account\n[3] Exit bank";
+    string whichAccount = "[1] Checking Account\n[2] Savings Account";
+    string accountInterface = "[1] Make a deposit\n[2] Make a Withdrawl";
+    incrementActNum( acctVect[acctVect.size()-1].chk.getActNum() );
+
+    cout << INITIAL_ACCT_NUM << endl;
+
+    /*while (true) //main while loop
+    {
+        cout << mainInterface << endl;
+
+    }*/
+
+
+
     return 0;
 }
--- OS.h
+++ OS.h
@@ -14,6 +14,24 @@ struct Accounts
     SavingsAccount sav;
 };
 
+string displayNum(double input);
+string getAccountInfo(Accounts accts);
+bool validateInput(string inputNum);
+int findAcctIndex(vector<Accounts> vect, string actNum);
+void incrementActNum(string lastActNum);
+vector<Accounts> getInfo();
+void saveInfo(vector<Accounts> acctVect);
+CheckingAccount createChkFromInfo(string info);
+SavingsAccount createSavFromInfo(string info);
+
+string INITIAL_ACCT_NUM = "00001"; //global variable for acct num
+
+string displayNum(double input)
+{
+    string num = to_string(input);
+    return num.substr(0, num.length() - 4); //cuts off the last 0000 of the double
+}
+
 string getAccountInfo(Accounts accts)
 {
     string chkInfo = "", savInfo;
@@ -31,6 +49,114 @@ string getAccountInfo(Accounts accts)
     
 
     return chkInfo + "\n" + savInfo;
+}
+
+bool validateInput(string inputNum)
+{
+    double num = stod(inputNum);
+    if (num < 0.01) //cannot withdraw or deposit less than a cent
+    {
+        return false;
+    }
+    else
+    {
+        if (num != BankAccount::roundNum(num, 2)) //e.g. depositing 10.056 isn't the same as 10.05
+        {
+            return false;
+        }
+        else //if a rounded double to 2 decimals is the same as the input, then the number is valid!
+        {
+            return true;
+        }
+    }    
+}
+
+int findAcctIndex(vector<Accounts> vect, string actNum)
+{
+    for (int i = 0; i < vect.size(); i++)
+    {
+        if ( (actNum == vect[i].chk.getActNum()) || (actNum == vect[i].sav.getActNum()) )
+        {
+            return i;
+        }
+    }
+    return -1; //only hit if never found act num
+}
+
+void incrementActNum(string lastActNum = "")
+{
+    int num = 0;
+    if (lastActNum == "")
+    {
+        num = stoi(INITIAL_ACCT_NUM);
+    }
+    else
+    {
+        num = stoi( lastActNum.substr(1, string::npos) ); //used if there are currently existing accts
+    }
+    
+    int count = 0;
+    int numCopy = num; //copying value into new var
+    while (numCopy >= 1)
+    {
+        numCopy /= 10;
+        count++; //count, upon end of while loop, will represent the highest power of 10, i.e. whether in the ones, tens, hundreds, etc
+    }
+    
+    string firstPartOfNum = "";
+    while ( (firstPartOfNum.length() + count) < 5) //five digits total in act num template
+    {
+        firstPartOfNum += "0";
+    }
+    num += 1;
+
+    INITIAL_ACCT_NUM = firstPartOfNum + to_string(num);
+}
+
+vector<Accounts> getInfo()
+{
+    vector<Accounts> acctVect = {};
+    ifstream inputFile;
+    inputFile.open("TotallyNotBankInfo.txt");
+
+    if (!inputFile) //i.e. no bank info to read
+    {
+        return acctVect;
+    }
+
+    else  //i.e. there is bank info saved
+    {
+        string chkLine = "", savLine = "";
+
+        while (getline(inputFile, chkLine))
+        {
+            getline(inputFile, savLine);
+            Accounts accSet; //now need to copy data froms strings into struct attributes
+            CheckingAccount check = createChkFromInfo(chkLine); //creates checking act from line
+            SavingsAccount saving = createSavFromInfo(savLine); //same with next line
+
+            accSet.chk = check; //adding new accts to struct
+            accSet.sav = saving;
+
+            acctVect.push_back(accSet); //adding the struct object to the vector
+        }
+
+        inputFile.close(); //done reading, so this can be closed
+        return acctVect;
+    }
+}
+
+void saveInfo(vector<Accounts> acctVect)
+{
+    ofstream outFile;
+    outFile.open("TotallyNotBankInfo.txt");
+
+    for (int i=0; i < acctVect.size(); i++)
+    {
+        outFile << getAccountInfo(acctVect[i]) << endl;
+    }
+
+    outFile.close();
 }
 
 CheckingAccount createChkFromInfo(string info)
@@ -80,72 +206,6 @@ SavingsAccount createSavFromInfo(string info)
         }
         return acct;
     }
-}
-
-vector<Accounts> getInfo()
-{
-    vector<Accounts> acctVect = {};
-    ifstream inputFile;
-    inputFile.open("TotallyNotBankInfo.txt");
-
-    if (!inputFile) //i.e. no bank info to read
-    {
-        return acctVect;
-    }
-
-    else  //i.e. there is bank info saved
-    {
-        string chkLine = "", savLine = "";
-
-        while (getline(inputFile, chkLine))
-        {
-            getline(inputFile, savLine);
-            Accounts accSet; //now need to copy data froms strings into struct attributes
-            CheckingAccount check = createChkFromInfo(chkLine); //creates checking act from line
-            SavingsAccount saving = createSavFromInfo(savLine); //same with next line
-
-            accSet.chk = check; //adding new accts to struct
-            accSet.sav = saving;
-
-            acctVect.push_back(accSet); //adding the struct object to the vector
-        }
-
-        inputFile.close(); //done reading, so this can be closed
-        return acctVect;
-    }
-}
-
-void saveInfo(vector<Accounts> acctVect)
-{
-    ofstream outFile;
-    outFile.open("TotallyNotBankInfo.txt");
-
-    for (int i=0; i < acctVect.size(); i++)
-    {
-        outFile << getAccountInfo(acctVect[i]) << endl;
-    }
-
-    outFile.close();
-}
-
-bool validateInput(string inputNum)
-{
-    double num = stod(inputNum);
-    if (num < 0.01) //cannot withdraw or deposit less than a cent
-    {
-        return false;
-    }
-    else
-    {
-        if (num != BankAccount::roundNum(num, 2)) //e.g. depositing 10.056 isn't the same as 10.05
-        {
-            return false;
-        }
-        else //if a rounded double to 2 decimals is the same as the input, then the number is valid!
-        {
-            return true;
-        }
-    }    
 }
 
 #endif
